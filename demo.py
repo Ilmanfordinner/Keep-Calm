a1bb8acdd60a5df36a9d730cee7129756ad2db25
Ilia Manolov <iliamanolovuni@gmail.com>
2017-11-25 11:37:08 +0000

    Initial commit

---
+++ demo.py
@@ -0,0 +1,26 @@
+import http.client, urllib.request, urllib.parse, urllib.error, base64, sys
+
+headers = {
+    # Request headers. Replace the placeholder key below with your subscription key.
+    'Content-Type': 'application/json',
+    'Ocp-Apim-Subscription-Key': 'd0bfdec61a7841609a35fda0c12d7788',
+}
+
+params = urllib.parse.urlencode({
+})
+
+# Replace the example URL below with the URL of the image you want to analyze.
+body = "{ 'url': 'http://i.telegraph.co.uk/multimedia/archive/03491/Vladimir_Putin_1_3491835k.jpg' }"
+
+try:
+    # NOTE: You must use the same region in your REST call as you used to obtain your subscription keys.
+    #   For example, if you obtained your subscription keys from westcentralus, replace "westus" in the 
+    #   URL below with "westcentralus".
+    conn = http.client.HTTPSConnection('westus.api.cognitive.microsoft.com')
+    conn.request("POST", "/emotion/v1.0/recognize?%s" % params, body, headers)
+    response = conn.getresponse()
+    data = response.read()
+    print(data)
+    conn.close()
+except Exception as e:
+    print(e.args)
