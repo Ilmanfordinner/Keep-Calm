de231699897eecd6448141b28c28908406e2c317
Bozhidar Vasilev <bojidar.b.vasilev@gmail.com>
2017-11-26 02:59:00 +0000

    Added callibration and persistent faces

--- KeepCalm.py
+++ KeepCalm.py
@@ -25,7 +25,11 @@ _face_cascade = None
 _global_edited_webcam_frame = None
 
 
-def processRequest( json, data, headers, params ):
+_players_array = None
+_players_override = None
+_persistent_faces = False
+
+def processRequest(json, data, headers, params):
 
     """
     Helper function to process the request to Project Oxford
@@ -41,7 +45,7 @@ def processRequest( json, data, headers, params ):
 
     while True:
 
-        response = requests.request( 'post', _url, json = json, data = data, headers = headers, params = params )
+        response = requests.request('post', _url, json = json, data = data, headers = headers, params = params )
 
         if response.status_code == 429: 
 
@@ -71,7 +75,7 @@ def processRequest( json, data, headers, params ):
         break
         
     return result
-	
+    
 def BGR2RGB(array):
     return np.roll(array, 1, axis=-1)
             
@@ -133,7 +137,7 @@ def drawFaceInfo(info, location, target):
     neutralness = info['scores']['neutral']
     
     green = (0,255,0)
-    print(location)
+    #print(location)
     
     x, y, w, h = location
     
@@ -153,30 +157,46 @@ def drawImage(s_img, l_img, x_offset, y_offset):
     for c in range(0, 3):
         l_img[y1:y2, x1:x2, c] = (alpha_s * s_img[:, :, c] + alpha_l * l_img[y1:y2, x1:x2, c])
         
-        
-def drawEmoji(info, location, target):
-    
-    if info['scores']['neutral'] > 0.5:
-        return
+def updateNearestPlayer(location, emoji):
+    x, y, w, h = location
+    to_update = 0
+    for index in range(1, len(_players_array)):
+        dist1 = abs(_players_array[to_update][0] - x)
+        dist2 = abs(_players_array[index][0] - x)
+        if dist2 < dist1:
+            to_update = index
+
+    _players_override[to_update] = emoji
+
+def drawEmoji(info, location, target, override_emoji=''):
         
     best = ('kek', -1)
     for f, s in info['scores'].items():
         if s > best[1]:
             best = (f, s)
-            
-    if best[0] == 'anger' or best[0] == 'contempt' or best[0] == 'disgust':
+    
+    #print(type(best[0]))
+    emotion = best[0]
+    if _persistent_faces and override_emoji != '':
+        emotion = override_emoji
+
+    #print(type(best))
+    if emotion == 'anger' or emotion == 'contempt' or emotion == 'disgust':
         emoji_raw = cv2.imread("angry_emoji.png", -1)
-    elif best[0] == 'fear' or best[0] == 'surprise':
+    elif emotion == 'fear' or emotion == 'surprise':
         emoji_raw = cv2.imread("fear_emoji.png", -1)
-    elif best[0] == 'happiness':    
+    elif emotion == 'happiness':    
         emoji_raw = cv2.imread("laughing_crying_emoji2.png", -1)
-    else:
+    elif emotion == 'sadness':
         emoji_raw = cv2.imread("sad_emoji.png", -1)
+    else:
+        return ''
     
     
     x, y, w, h = location
     emoji = cv2.resize(emoji_raw, (w, h))
     drawImage(emoji, target, x, y)
+    return emotion
 
         
 def drawInfo(frame):
@@ -191,9 +211,34 @@ def drawInfo(frame):
     
     face_locations = sorted(face_locations, key=lambda loc: loc[0])
     
+    #print(_players_array)
     for index in range(0, min(len(tmp_response), len(face_locations))):
-        drawEmoji(tmp_response[index], face_locations[index], frame)
+        print(_players_override)
+        res = drawEmoji(tmp_response[index], face_locations[index], frame, _players_override[index])
+        if res != '':
+            updateNearestPlayer(face_locations[index], res)
+
+def drawRects(frame):
+        
+    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
+    face_locations = _face_cascade.detectMultiScale(grayscale, scaleFactor = 1.1, minNeighbors = 5, minSize=(30, 30), flags=cv2.CASCADE_SCALE_IMAGE)
+
+    tmp_response = _latest_response
+    
+    tmp_response = sorted(tmp_response, key=lambda face: face['faceRectangle']['left'])
+    
+    face_locations = sorted(face_locations, key=lambda loc: loc[0])
+
+    global _players_array
+    global _players_override
+    _players_array = face_locations
+    _players_override = [''] * len(face_locations)
+    print(_players_override)
+    
+    for index in range(0, min(len(tmp_response), len(face_locations))):
+        drawFaceInfo(tmp_response[index], face_locations[index], frame)
+
 
 def webcamThread():
     
@@ -209,8 +254,46 @@ def webcamThread():
         
         _global_edited_webcam_frame = edited_frame
     
-   
+
+def initialize():
+    cap = cv2.VideoCapture(0)
+    
+    global _face_cascade
+    global _current_frame
+    
+    cascPath = "haarcascade_frontalface_default.xml"
+    _face_cascade = cv2.CascadeClassifier(cascPath)
+
+    _lastRequestTime = time.time()
+    
+    t = threading.Thread(target = requestThread)
+    t.setDaemon(True)
+    t.start()
+
+    
+    while(True):
+        # Capture frame-by-frame
+        ret, _current_frame = cap.read()
+ 
+        # Our operations on the frame come her
+        
+        color = cv2.resize(cv2.cvtColor(_current_frame, cv2.IMREAD_COLOR), (0, 0), fx = 0.5, fy = 0.5)
+        drawRects(_current_frame)
+    
+        # Display the resulting frame
+        cv2.imshow('edited_frame',_current_frame)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+            
+    
+    # When everything done, release the capture
+    cap.release()
+    cv2.destroyAllWindows()
+
+
 def main():
+    initialize()
+
     cap = cv2.VideoCapture(0)
     videocap = cv2.VideoCapture('1.mp4')
     
@@ -244,7 +327,6 @@ def main():
         if not _global_edited_webcam_frame is None:
             color = cv2.resize(cv2.cvtColor(_global_edited_webcam_frame, cv2.IMREAD_COLOR), (0, 0), fx = 0.5, fy = 0.5)
             drawImage(color, video_frame, 0, 0)
-            print ("kek")
         
     
         # Display the resulting frame
@@ -255,6 +337,7 @@ def main():
     
     # When everything done, release the capture
     cap.release()
+    videocap.release()
     cv2.destroyAllWindows()
 
 
